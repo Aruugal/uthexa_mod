ae8513c38b60d02a0fb71066d5816e0916c8949b
Yann <popof@way-net.eu>
2012-10-11 23:32:27 +0200

    Crash Russe

---
+++ uthexa/config.cpp
@@ -0,0 +1,26 @@
+﻿class CfgVehicles {
+	class House {
+		class DestructionEffects;	// External class reference
+	};
+	
+	class UH1Wreck_HEX : House {
+		model = "\ca\air2\UH1Y\UH1Y_Crashed.p3d";
+		icon = "\ca\air2\data\UI\icon_UH1Y_CA.paa";
+		mapSize = 15;
+		displayName = "Crashed UH-1Y";
+		vehicleClass = "Wrecks";
+	};
+};
+class CfgBuildingLoot {
+	class Default;
+	class Military;	// External class reference
+	
+	class UH1Wreck_HEX : Military {
+		zombieClass[] = {"z_soldier_pilot", "z_soldier_heavy"};
+		zombieChance = 0;
+		lootChance = 0;
+		minRoaming = 4;
+		maxRoaming = 8;
+		lootPos[] = {};
+	};
+};
