81d4dd096266b771197ef687473a5e056a28bd95
Yann <popof@way-net.eu>
2012-10-12 15:48:59 +0200

    Zombies au site de ravitaillement

--- uthexa/config.cpp
+++ uthexa/config.cpp
@@ -9,16 +9,38 @@
 class CfgVehicles {
 	class House;
 	class Mi8Wreck_HEX : House {
-		model = "\ca\air\Mi8Wreck.p3d"";
+		model = "\ca\air\Mi8Wreck.p3d";
 		icon = "\ca\air2\data\UI\icon_UH1Y_CA.paa";
 		mapSize = 15;
 		displayName = "Crashed Mi8";
+		vehicleClass = "Wrecks";
+	};
+	class Cargo1_HEX : House {
+		model = "\ca\misc\Misc_cargo_cont_net1.p3d";
+		icon = "\ca\air2\data\UI\icon_UH1Y_CA.paa";
+		mapSize = 15;
+		displayName = "Cargo1";
+		vehicleClass = "Wrecks";
+	};
+	class Cargo2_HEX : House {
+		model = "\ca\misc\Misc_cargo_cont_net2.p3d";
+		icon = "\ca\air2\data\UI\icon_UH1Y_CA.paa";
+		mapSize = 15;
+		displayName = "Cargo2";
+		vehicleClass = "Wrecks";
+	};
+	class Cargo3_HEX : House {
+		model = "\ca\misc\Misc_cargo_cont_net3.p3d";
+		icon = "\ca\air2\data\UI\icon_UH1Y_CA.paa";
+		mapSize = 15;
+		displayName = "Cargo3";
 		vehicleClass = "Wrecks";
 	};
 };
 class CfgBuildingLoot {
 	class Default;
 	class Military;
+	class Residential;
 	class Mi8Wreck_HEX : Military {
 		zombieClass[] = {"z_soldier_pilot", "z_soldier_heavy"};
 		zombieChance = 0;
@@ -27,4 +49,26 @@ class CfgBuildingLoot {
 		maxRoaming = 8;
 		lootPos[] = {};
 	};
+	class Cargo1_HEX : Residential {
+		zombieChance = 0;
+		minRoaming = 1;
+		maxRoaming = 3;
+		lootChance = 0;
+		lootPos[] = {};
+	};
+	class Cargo2_HEX : Residential {
+		zombieChance = 0;
+		minRoaming = 3;
+		maxRoaming = 6;
+		lootChance = 0;
+		lootPos[] = {};
+	};
+	class Cargo3_HEX : Residential {
+		zombieChance = 0;
+		minRoaming = 4;
+		maxRoaming = 8;
+		lootChance = 0;
+		lootPos[] = {};
+	};
+	
 };
